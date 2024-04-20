153625fce7a5c911dfc5b67d86a719a0eda0ffa6
EricYanNaing <ericthedev97@gmail.com>
2024-04-20 11:13:21 +0700

    commit

---
+++ src/components/BackDrop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Cart from "./Cart";
+
+const BackDrop = () => {
+  return (
+    <>
+      <section className="backDrop">
+        <Cart />
+      </section>
+    </>
+  );
+};
+
+export default BackDrop;
---
+++ src/components/Cart.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+const frutis = [
+  {
+    id: 1,
+    price: 9.99,
+    name: "Banana",
+    description: "Fresh Banana From Thailand",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    price: 7.99,
+    name: "Apple",
+    description: "Fresh Apple From Thailand",
+    quantity: 3,
+  },
+  {
+    id: 3,
+    price: 8.99,
+    name: "Mango",
+    description: "Fresh Mango From Thailand",
+    quantity: 2,
+  },
+];
+
+const Cart = () => {
+  return (
+    <section className="cart-box">
+      <h2>Carts</h2>
+      {frutis.map((fruit) => (
+        <CartItem fruit={fruit} key={fruit.id} />
+      ))}
+      <hr />
+      <div className="total">
+        <h1>Total Price</h1>
+        <p>$ 400.00</p>
+      </div>
+      <div className="btns">
+        <button className="close-btn">Close</button>
+        <button className="order-btn">Go To Order</button>
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
